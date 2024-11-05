224b41ce88b2e6d981670067fac9af6c348f85a4
pglob <globischpaul@gmail.com>
2024-11-04 21:46:41 -0800

    Setup initial backend (CSCAP-31)

---
+++ backend/src/main/java/com/sim_backend/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package contains the backend logic for the EV Charger Simulator.
+ */
+package com.sim_backend;
---
+++ backend/src/main/java/com/sim_backend/rest/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains the REST api controllers for communicating with the
+ * simulator front end.
+ */
+package com.sim_backend.rest;
