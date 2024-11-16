209d1b9a2af3e551afb0b3731073e5275a78eb24
cfarhan <cameronfarhan2@gmail.com>
2024-11-15 23:17:06 -0800

    Update message ID generation to use UUID

--- backend/src/main/java/com/sim_backend/websockets/OCPPMessage.java
+++ backend/src/main/java/com/sim_backend/websockets/OCPPMessage.java
@@ -1,12 +1,10 @@
 package com.sim_backend.websockets;
 
 import com.google.gson.JsonArray;
-import java.security.SecureRandom;
+import java.util.UUID;
 import org.jetbrains.annotations.NotNull;
 
 public abstract class OCPPMessage {
-  /** The maximum allowed message ID length. */
-  public static final int MAX_MESSAGE_ID_LENGTH = 20;
 
   /** The call ID for a request. */
   public static final int CALL_ID_REQUEST = 2;
@@ -60,25 +58,13 @@ public abstract class OCPPMessage {
     return tries;
   }
 
-  /** The Characters we are allowed in a Message ID. */
-  private static final String CHARACTERS =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-  /** Our random generator. */
-  private static final SecureRandom RANDOM = new SecureRandom();
-
   /**
-   * Generates a message ID of a given length.
+   * Generates a unique 36 character UUID
    *
    * @return A randomly generated message ID.
    */
   @NotNull
   private static String generateMessageID() {
-    StringBuilder sb = new StringBuilder(OCPPMessage.MAX_MESSAGE_ID_LENGTH);
-    for (int i = 0; i < OCPPMessage.MAX_MESSAGE_ID_LENGTH; i++) {
-      int randomIndex = RANDOM.nextInt(CHARACTERS.length());
-      sb.append(CHARACTERS.charAt(randomIndex));
-    }
-    return sb.toString();
+    return UUID.randomUUID().toString();
   }
 }
