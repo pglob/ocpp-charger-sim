4c4885b3a5b83ba5db95dacc968784695a3f9139
cfarhan <cameronfarhan2@gmail.com>
2024-11-16 18:14:52 -0800

    Create Authorize response and request

---
+++ backend/src/main/java/com/sim_backend/websockets/messages/Authorize.java
@@ -0,0 +1,40 @@
+package com.sim_backend.websockets.messages;
+
+import com.google.gson.annotations.SerializedName;
+import com.sim_backend.websockets.OCPPMessage;
+import com.sim_backend.websockets.OCPPMessageInfo;
+
+import java.util.UUID;
+
+@OCPPMessageInfo(messageCallID = OCPPMessage.CALL_ID_REQUEST, messageName = "Authorize")
+public class Authorize extends OCPPMessage {
+
+  @SerializedName("idTag")
+  private String idTag;
+
+  // Constructor
+  public Authorize(String idTag) {
+      this.idTag = idTag;
+  }
+
+  // Constructor
+  public Authorize() {
+    this.idTag = generateIdTag();
+  }
+
+  // Getter and Setter
+  public String getIdTag() {
+      return idTag;
+  }
+
+  public void setIdTag(String idTag) {
+      this.idTag = idTag;
+  }
+
+  public String generateIdTag() {
+    // Generate a UUID
+    String uuid = UUID.randomUUID().toString();
+    // Remove hyphens and truncate to 20 characters
+    return uuid.replace("-", "").substring(0, 20);
+  }
+}
---
+++ backend/src/main/java/com/sim_backend/websockets/messages/AuthorizeResponse.java
@@ -0,0 +1,27 @@
+package com.sim_backend.websockets.messages;
+
+import com.google.gson.annotations.SerializedName;
+import com.sim_backend.websockets.OCPPMessage;
+import com.sim_backend.websockets.OCPPMessageInfo;
+
+@OCPPMessageInfo(messageCallID = OCPPMessage.CALL_ID_RESPONSE, messageName = "AuthorizeResponse")
+public class AuthorizeResponse extends OCPPMessage {
+
+    @SerializedName("status")
+    private String status;
+
+    // Constructor
+    public AuthorizeResponse(String status) {
+        this.status = status;
+    }
+
+    // Getter
+    public String getStatus() {
+        return status;
+    }
+
+    // Setter
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
