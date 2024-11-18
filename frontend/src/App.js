bf5e7c94d8eb1382ce42637e8c40d913ea501263
sille369 <97776740+sille369@users.noreply.github.com>
2024-11-17 22:20:26 -0800

    Added testing framework to frontend * Added jest and RTL framework to frontend * Set up a dummy test for verifing the api call from backend of the App.js * Modified the frontend/Dockerfile and docker-compose.yml

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,16 +6,16 @@ function App() {
   // Hook to fetch data from backend when the component mounts
   useEffect(() => {
     fetch(`${process.env.REACT_APP_BACKEND_URL}/api/test`)
-      .then(response => response.text())
-      .then(data => setMessage(data))
-      .catch(error => console.error('Error fetching test message:', error));
+      .then((response) => response.text())
+      .then((data) => setMessage(data))
+      .catch((error) => console.error('Error fetching test message:', error));
   }, []);
 
   // Render the component
   return (
     <div>
       <h1>Frontend</h1>
-      <p>Message from backend: {message}</p>
+      <p data-testid="message">Message from backend: {message}</p>
     </div>
   );
 }
