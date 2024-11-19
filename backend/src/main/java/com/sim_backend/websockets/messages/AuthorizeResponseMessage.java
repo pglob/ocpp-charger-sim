81ae0c998e36d99a5ed0cd8f7e45e914718f2cc0
cfarhan <cameronfarhan2@gmail.com>
2024-11-19 00:03:29 -0800

    Update class names for clarity

---
+++ backend/src/main/java/com/sim_backend/websockets/messages/AuthorizeResponseMessage.java
@@ -0,0 +1,28 @@
+package com.sim_backend.websockets.messages;
+
+import com.google.gson.annotations.SerializedName;
+import com.sim_backend.websockets.OCPPMessage;
+import com.sim_backend.websockets.OCPPMessageInfo;
+
+@OCPPMessageInfo(messageCallID = OCPPMessage.CALL_ID_RESPONSE, messageName = "AuthorizeResponse")
+public class AuthorizeResponseMessage extends OCPPMessage {
+
+  @SerializedName("status")
+  private String status;
+
+  // Constructor
+  public AuthorizeResponseMessage(String status) {
+    super();
+    this.status = status;
+  }
+
+  // Getter
+  public String getStatus() {
+    return status;
+  }
+
+  // Setter
+  public void setStatus(String status) {
+    this.status = status;
+  }
+}
