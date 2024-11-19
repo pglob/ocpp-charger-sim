7aeb32ad9c24935e5d8ee579ceb8cfac0de1c699
cfarhan <cameronfarhan2@gmail.com>
2024-11-19 00:19:01 -0800

    Remove unneeded import

--- backend/src/main/java/com/sim_backend/websockets/messages/HeartBeatResponseMessage.java
+++ backend/src/main/java/com/sim_backend/websockets/messages/HeartBeatResponseMessage.java
@@ -1,6 +1,5 @@
 package com.sim_backend.websockets.messages;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.sim_backend.websockets.OCPPMessage;
 import com.sim_backend.websockets.OCPPMessageInfo;
 import java.time.ZoneId;
