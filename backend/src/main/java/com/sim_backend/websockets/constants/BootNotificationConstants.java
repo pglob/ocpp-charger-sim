01c913a58fe140777b6cd4d1fbb6f68468b11a3e
cfarhan <cameronfarhan2@gmail.com>
2024-12-04 20:38:39 -0800

    Create BootNotificationConstants

---
+++ backend/src/main/java/com/sim_backend/websockets/constants/BootNotificationConstants.java
@@ -0,0 +1,18 @@
+package com.sim_backend.websockets.constants;
+
+public final class BootNotificationConstants {
+
+  private BootNotificationConstants() {
+    // Private constructor to prevent instantiation
+  }
+
+  public static final String CHARGE_POINT_VENDOR = "SimulareVendor";
+  public static final String CHARGE_POINT_MODEL = "ModelForPSUCapstone";
+  public static final String CHARGE_POINT_SERIAL_NUMBER = "CPSN123456789012345";
+  public static final String CHARGE_BOX_SERIAL_NUMBER = "CBSN123456789012345";
+  public static final String FIRMWARE_VERSION = "FW_1.0.0_Version_2024";
+  public static final String ICCID = "ICCID1234567890123";
+  public static final String IMSI = "IMSI1234567890123";
+  public static final String METER_TYPE = "MeterType123";
+  public static final String METER_SERIAL_NUMBER = "MSN123456789012345";
+}
