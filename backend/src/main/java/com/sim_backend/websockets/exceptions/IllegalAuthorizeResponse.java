5a3d24afb5c27c2aac3be0d3364421394e459955
JayQuack <95886074+JayQuack@users.noreply.github.com>
2024-12-10 09:17:26 -0800

    Delete Exception File

--- backend/src/main/java/com/sim_backend/websockets/exceptions/IllegalAuthorizeResponse.java
+++
@@ -1,7 +0,0 @@
-package com.sim_backend.websockets.exceptions;
-
-public class IllegalAuthorizeResponse extends RuntimeException {
-  public IllegalAuthorizeResponse(String message) {
-    super(message);
-  }
-}
