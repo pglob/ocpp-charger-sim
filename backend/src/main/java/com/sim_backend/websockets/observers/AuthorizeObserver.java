7e0f63b124fbfd5858d6cffcaccb82c9170b90cb
JayQuack <95886074+JayQuack@users.noreply.github.com>
2024-12-10 10:27:27 -0800

    Add AuthorizeObserverTest

--- backend/src/main/java/com/sim_backend/websockets/observers/AuthorizeObserver.java
+++ backend/src/main/java/com/sim_backend/websockets/observers/AuthorizeObserver.java
@@ -5,38 +5,38 @@ import com.sim_backend.websockets.enums.AuthorizationStatus;
 import com.sim_backend.websockets.events.*;
 import com.sim_backend.websockets.messages.*;
 
-public class AuthorizeObserver implements OnOCPPMessageListener{
+public class AuthorizeObserver implements OnOCPPMessageListener {
 
-    public void handleAuthorize(OCPPWebSocketClient client, AuthorizeResponse response){
-        if(response.getIdTagInfo().getStatus() == AuthorizationStatus.ACCEPTED){
-            client.pushMessage(new AuthorizeResponse(response.getIdTagInfo()));
-        } else {
-            System.out.println("Failed Authorize");
-        }
+  public void handleAuthorize(OCPPWebSocketClient client, AuthorizeResponse response) {
+    if (response.getIdTagInfo().getStatus() == AuthorizationStatus.ACCEPTED) {
+      client.pushMessage(new AuthorizeResponse(response.getIdTagInfo()));
+    } else {
+      System.out.println("Failed Authorize");
     }
+  }
 
-    @Override
-    public void onMessageReceived(OnOCPPMessage message) {
-        if(!(message.getMessage() instanceof AuthorizeResponse response)){
-            throw new ClassCastException("Message is not an AuthorizeResponse");            
-        } 
-        switch(response.getIdTagInfo().getStatus()){
-            case ACCEPTED:
-                System.out.println("Authorization Accepted.");
-                break;
-            case BLOCKED:
-                System.err.println("Authorization Blocked for ID.");
-                break;
-            case EXPIRED:
-                System.err.println("Authorization Expired for ID.");
-                break;
-            case INVALID:
-                System.err.println("Invalid ID.");
-                break;
-            case CONCURRENT:
-                System.err.println("Concurrent Transaction Occured.");
-            default:
-            System.err.println("Unknown Status Received.");
-        }
+  @Override
+  public void onMessageReceived(OnOCPPMessage message) {
+    if (!(message.getMessage() instanceof AuthorizeResponse response)) {
+      throw new ClassCastException("Message is not an AuthorizeResponse");
     }
+    switch (response.getIdTagInfo().getStatus()) {
+      case ACCEPTED:
+        System.out.println("Authorization Accepted.");
+        break;
+      case BLOCKED:
+        System.err.println("Authorization Blocked for ID.");
+        break;
+      case EXPIRED:
+        System.err.println("Authorization Expired for ID.");
+        break;
+      case INVALID:
+        System.err.println("Invalid ID.");
+        break;
+      case CONCURRENT:
+        System.err.println("Concurrent Transaction Occured.");
+      default:
+        System.err.println("Unknown Status Received.");
+    }
+  }
 }
