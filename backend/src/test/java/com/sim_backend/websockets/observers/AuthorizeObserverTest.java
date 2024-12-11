686b6892f442df13023b7c485a268cf41ebf73ad
JayQuack <95886074+JayQuack@users.noreply.github.com>
2024-12-10 17:14:35 -0800

    Add Javadoc, Delete Unnecessary Code Block

--- backend/src/test/java/com/sim_backend/websockets/observers/AuthorizeObserverTest.java
+++ backend/src/test/java/com/sim_backend/websockets/observers/AuthorizeObserverTest.java
@@ -2,7 +2,6 @@ package com.sim_backend.websockets.observers;
 
 import static org.mockito.Mockito.*;
 
-import com.sim_backend.websockets.OCPPWebSocketClient;
 import com.sim_backend.websockets.events.OnOCPPMessage;
 import com.sim_backend.websockets.messages.AuthorizeResponse;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,7 +9,6 @@ import org.junit.jupiter.api.Test;
 import org.mockito.*;
 
 public class AuthorizeObserverTest {
-  @Mock private OCPPWebSocketClient testClient;
   @Mock private OnOCPPMessage testMessage;
 
   private AuthorizeObserver testObserver;
@@ -19,47 +17,6 @@ public class AuthorizeObserverTest {
   void setUp() {
     MockitoAnnotations.openMocks(this);
     testObserver = new AuthorizeObserver();
-  }
-
-  @Test
-  void testAuthorizeAccepted() {
-    AuthorizeResponse response = new AuthorizeResponse("Accepted");
-    testObserver.handleAuthorize(testClient, response);
-
-    ArgumentCaptor<AuthorizeResponse> captor = ArgumentCaptor.forClass(AuthorizeResponse.class);
-    verify(testClient).pushMessage(captor.capture());
-  }
-
-  @Test
-  void testAuthorizeBlocked() {
-    AuthorizeResponse response = new AuthorizeResponse("Blocked");
-    testObserver.handleAuthorize(testClient, response);
-
-    verify(testClient, never()).pushMessage(any(AuthorizeResponse.class));
-  }
-
-  @Test
-  void testAuthorizeExpired() {
-    AuthorizeResponse response = new AuthorizeResponse("Expired");
-    testObserver.handleAuthorize(testClient, response);
-
-    verify(testClient, never()).pushMessage(any(AuthorizeResponse.class));
-  }
-
-  @Test
-  void testAuthorizeInvalid() {
-    AuthorizeResponse response = new AuthorizeResponse("Invalid");
-    testObserver.handleAuthorize(testClient, response);
-
-    verify(testClient, never()).pushMessage(any(AuthorizeResponse.class));
-  }
-
-  @Test
-  void testAuthorizeConcurrent() {
-    AuthorizeResponse response = new AuthorizeResponse("ConcurrentTx");
-    testObserver.handleAuthorize(testClient, response);
-
-    verify(testClient, never()).pushMessage(any(AuthorizeResponse.class));
   }
 
   @Test
