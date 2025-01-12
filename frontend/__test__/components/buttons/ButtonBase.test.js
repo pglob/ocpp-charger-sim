5e927d0c1404ef718f03c3145389d79c8220a37d
zhihw <150119423+zhihw@users.noreply.github.com>
2025-01-12 15:33:59 -0800

    Cscap 76/handle text and add test (#41)

---
+++ frontend/__test__/components/buttons/ButtonBase.test.js
@@ -0,0 +1,64 @@
+import ButtonBase from '../../../src/compenents/buttons/ButtonBase';
+
+beforeEach(() => {
+    global.fetch = jest.fn();
+});
+    
+afterEach(() => {
+    jest.clearAllMocks();
+});
+
+describe('ButtonBase PostRequest', () => {
+    const TestButton = new ButtonBase('TestButton', '/test-endpoint'); 
+    it('should correctly handle text response', async () => {
+        const mockTextResponse = 'This is a test text'; 
+        global.fetch.mockResolvedValueOnce({
+            ok: true, 
+            headers: {
+                get: () => 'text/plain', 
+            },
+            text: () => Promise.resolve(mockTextResponse), 
+        });
+
+        const response = await TestButton.postRequest({ key: 'data' }); 
+
+        expect(response).toBe(mockTextResponse); 
+        expect(global.fetch).toHaveBeenCalledWith(
+            `${process.env.REACT_APP_BACKEND_URL}/test-endpoint`, 
+            {
+                method: 'POST', 
+                headers: {
+                    'Content-Type': 'application/json', 
+                },
+                body: JSON.stringify({ key: 'data' }), 
+            }
+        );
+    });
+
+    it('should correctly handle JSON response', async () => {
+        const mockJsonResponse = { message: 'Success' }; 
+
+        global.fetch.mockResolvedValueOnce({
+            ok: true, 
+            headers: {
+                get: () => 'application/json', 
+            },
+            json: () => Promise.resolve(mockJsonResponse), 
+        });
+
+        const response = await TestButton.postRequest({ key: 'data' }); 
+
+        expect(response).toEqual(mockJsonResponse); 
+        expect(global.fetch).toHaveBeenCalledWith(
+            `${process.env.REACT_APP_BACKEND_URL}/test-endpoint`, 
+            {
+                method: 'POST', 
+                headers: {
+                    'Content-Type': 'application/json', 
+                },
+                body: JSON.stringify({ key: 'data' }), 
+            }
+        );
+    });
+});
+        
