8cbdb143faf80d7eb25196928ed8c5c71c2e604a
cfarhan <cameronfarhan2@gmail.com>
2025-01-20 17:31:30 -0800

    Spotless changes

--- backend/src/main/java/com/sim_backend/ConfigurationRegistry.java
+++ backend/src/main/java/com/sim_backend/ConfigurationRegistry.java
@@ -15,53 +15,53 @@ import lombok.Setter;
 @Setter
 public class ConfigurationRegistry {
 
-    /** ChangeConfiguration authorization key */
-    private String authKey;
+  /** ChangeConfiguration authorization key */
+  private String authKey;
 
-    /** ChangeConfiguration authorization key value */
-    private String authKeyValue;
+  /** ChangeConfiguration authorization key value */
+  private String authKeyValue;
 
-    /** Authorize response id tag */
-    private String idTag;
+  /** Authorize response id tag */
+  private String idTag;
 
-    /** The ChargeBox's Serial Number */
-    private String centralSystemUrl;
+  /** The ChargeBox's Serial Number */
+  private String centralSystemUrl;
 
-    /** The Charge Point's Vendor. */
-    private final String chargePointVendor = BootNotificationConstants.CHARGE_POINT_VENDOR;
+  /** The Charge Point's Vendor. */
+  private final String chargePointVendor = BootNotificationConstants.CHARGE_POINT_VENDOR;
 
-    /** The Charge Point's Model. */
-    private final String chargePointModel = BootNotificationConstants.CHARGE_POINT_MODEL;
+  /** The Charge Point's Model. */
+  private final String chargePointModel = BootNotificationConstants.CHARGE_POINT_MODEL;
 
-    /** The Charge Point's Serial Number. */
-    private final String chargePointSerialNumber =
-            BootNotificationConstants.CHARGE_POINT_SERIAL_NUMBER;
+  /** The Charge Point's Serial Number. */
+  private final String chargePointSerialNumber =
+      BootNotificationConstants.CHARGE_POINT_SERIAL_NUMBER;
 
-    /** The Charge Box's Serial Number. */
-    private final String chargeBoxSerialNumber = BootNotificationConstants.CHARGE_BOX_SERIAL_NUMBER;
+  /** The Charge Box's Serial Number. */
+  private final String chargeBoxSerialNumber = BootNotificationConstants.CHARGE_BOX_SERIAL_NUMBER;
 
-    /** Firmware Version. */
-    private final String firmwareVersion = BootNotificationConstants.FIRMWARE_VERSION;
+  /** Firmware Version. */
+  private final String firmwareVersion = BootNotificationConstants.FIRMWARE_VERSION;
 
-    /** ICCID (Integrated Circuit Card Identifier) . */
-    private final String iccid = BootNotificationConstants.ICCID;
+  /** ICCID (Integrated Circuit Card Identifier) . */
+  private final String iccid = BootNotificationConstants.ICCID;
 
-    /** IMSI (International Mobile Subscriber Identity). */
-    private final String imsi = BootNotificationConstants.IMSI;
+  /** IMSI (International Mobile Subscriber Identity). */
+  private final String imsi = BootNotificationConstants.IMSI;
 
-    /** Meter Type. */
-    private final String meterType = BootNotificationConstants.METER_TYPE;
+  /** Meter Type. */
+  private final String meterType = BootNotificationConstants.METER_TYPE;
 
-    /** Meter Serial Number. */
-    private final String meterSerialNumber = BootNotificationConstants.METER_SERIAL_NUMBER;
+  /** Meter Serial Number. */
+  private final String meterSerialNumber = BootNotificationConstants.METER_SERIAL_NUMBER;
 
-    /** A sample meter value interval. */
-    private int MeterValueSampleInterval = 30;
+  /** A sample meter value interval. */
+  private int MeterValueSampleInterval = 30;
 
-    /** Authorization Enable check. */
-    boolean AuthEnabled = true;
+  /** Authorization Enable check. */
+  boolean AuthEnabled = true;
 
-    /** Sample meter value data. */
-    private MeterValuesSampledData meterValuesSampledData =
-            MeterValuesSampledData.ENERGY_ACTIVE_IMPORT_REGISTER;
+  /** Sample meter value data. */
+  private MeterValuesSampledData meterValuesSampledData =
+      MeterValuesSampledData.ENERGY_ACTIVE_IMPORT_REGISTER;
 }
--- backend/src/main/java/com/sim_backend/websockets/messages/StatusNotificationResponse.java
+++ backend/src/main/java/com/sim_backend/websockets/messages/StatusNotificationResponse.java
@@ -2,11 +2,14 @@ package com.sim_backend.websockets.messages;
 
 import com.sim_backend.websockets.annotations.OCPPMessageInfo;
 import com.sim_backend.websockets.types.OCPPMessage;
+
 /**
  * Represents an OCPP 1.6 Status Notification Response sent by the Central System to acknowledge a
  * Status Notification Request.
  */
-@OCPPMessageInfo(messageCallID = OCPPMessage.CALL_ID_RESPONSE, messageName = "StatusNotificationResponse")
+@OCPPMessageInfo(
+    messageCallID = OCPPMessage.CALL_ID_RESPONSE,
+    messageName = "StatusNotificationResponse")
 public class StatusNotificationResponse {
-     // No fields are defined as per the protocol specification
+  // No fields are defined as per the protocol specification
 }
