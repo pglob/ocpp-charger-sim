10fc1c062fc73edf1781efee1316eab359520c80
cfarhan <cameronfarhan2@gmail.com>
2025-01-27 22:26:18 -0800

    Replace some variables with getters. Refactor transition method

--- backend/src/main/java/com/sim_backend/ElectricalTransition.java
+++ backend/src/main/java/com/sim_backend/ElectricalTransition.java
@@ -1,8 +1,7 @@
 package com.sim_backend;
 
-import com.sim_backend.state.IllegalStateException;
 import com.sim_backend.state.SimulatorState;
-import com.sim_backend.state.SimulatorStateMachine;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 /**
@@ -10,26 +9,18 @@ import lombok.NoArgsConstructor;
  * power, voltage, current, and energy consumed. It tracks the charging state and calculates the
  * power usage and energy consumption in kilowatt-hours (kWh).
  */
+@Getter
 @NoArgsConstructor
 public class ElectricalTransition {
 
-  /** The charger's voltage in volts. */
+  /** The charger's voltage in volts. Defaults to 240 volts */
   private int voltage;
 
-  /** The maximum current offered by the charger in amps. */
+  /** The maximum current offered by the charger in amps. Defaults to 40 amps */
   private int currentOffered;
 
-  /** The actual current drawn from the charger by the EV. */
+  /** The actual current drawn from the charger by the EV. Defaults to 40 amps */
   private int currentImport;
-
-  /** The maximum power the charger is capable of providing in kilowatts (kW). */
-  private float powerOffered;
-
-  /**
-   * The actual power consumed by the charger in kilowatts (kW). Value is not calculated using load
-   * balancing.
-   */
-  private float powerActiveImport;
 
   /** A timestamp of when charging first started. */
   private long initialChargeTimestamp;
@@ -41,13 +32,31 @@ public class ElectricalTransition {
   private static final long MILLISECONDS_PER_HOUR = 3600000;
 
   /**
+   * Calculates and returns the maximum power the charger is capable of providing in kilowatts (kW).
+   *
+   * @return the maximum power offered in kilowatts (kW).
+   */
+  public float getPowerOffered() {
+    return (float) (currentOffered * voltage) / 1000;
+  }
+
+  /**
+   * Calculates and returns the actual power consumed by the charger in kilowatts (kW).
+   *
+   * @return the actual power consumed in kilowatts (kW).
+   */
+  public float getPowerActiveImport() {
+    return (float) (currentImport * voltage) / 1000;
+  }
+
+  /**
    * Retrieves the amount of energy consumed since the specified interval in kilowatt-hours (kWh).
    *
    * @param interval the interval in seconds for which the energy usage is being queried
    * @return the energy consumed since the given interval in kilowatt-hours (kWh).
    */
   public float getEnergyActiveImportInterval(int interval) {
-    return this.powerOffered * ((float) interval / SECONDS_PER_HOUR);
+    return getPowerActiveImport() * ((float) interval / SECONDS_PER_HOUR);
   }
 
   /**
@@ -57,35 +66,33 @@ public class ElectricalTransition {
    */
   public float getEnergyActiveImportRegister() {
     long timeCharging = System.currentTimeMillis() - this.initialChargeTimestamp;
-    return this.powerOffered * ((float) timeCharging / MILLISECONDS_PER_HOUR);
+    return getPowerActiveImport() * ((float) timeCharging / MILLISECONDS_PER_HOUR);
   }
 
   /**
-   * Initiates the charging transition, setting the initial charging parameters such as voltage,
-   * current, and power. The function also checks if the state machine is in the correct state
-   * (Charging) before applying the transition.
+   * Updates the electrical values based on the given simulator state. If the state is not Charging,
+   * all electrical values are reset to zero. Otherwise, when in the Charging state, the voltage is
+   * set to 240V, and the current offered and imported are both set to 40A. The initial charge
+   * timestamp is also updated to the current system time.
    *
-   * @param stateMachine The state machine controlling the charging process.
-   * @throws IllegalStateException If the current state is not 'Charging', an exception is thrown.
+   * @param state the current state of the charger
    */
-  public void ChargingTransition(SimulatorStateMachine stateMachine) {
+  public void transition(SimulatorState state) {
 
-    if (stateMachine.getCurrentState() != SimulatorState.Charging) {
-      throw new IllegalStateException(
-          "Charging transition triggered during incorrect state: "
-              + stateMachine.getCurrentState());
+    // Whenever we aren't charging, zero all the electrical values.
+    if (state != SimulatorState.Charging) {
+      this.voltage = 0;
+      this.currentOffered = 0;
+      this.currentImport = 0;
+      this.initialChargeTimestamp = 0;
     }
 
-    // Set the charger's voltage and current parameters for the transition.
-    this.voltage = 240;
-    this.currentOffered = 40;
-    this.currentImport = 40;
-
-    // Calculate the power offered and the active power import in kilowatts.
-    this.powerOffered = (float) (currentOffered * voltage) / 1000;
-    this.powerActiveImport = (float) (currentImport * voltage) / 1000;
-
-    // Record the timestamp when the charging starts.
-    initialChargeTimestamp = System.currentTimeMillis();
+    // State is charging, proceed to set electrical values
+    else {
+      this.voltage = 240;
+      this.currentOffered = 40;
+      this.currentImport = 40;
+      this.initialChargeTimestamp = System.currentTimeMillis();
+    }
   }
 }
