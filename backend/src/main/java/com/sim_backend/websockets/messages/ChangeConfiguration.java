787dfee8c14bd9624fa79321ed6cd14c40aa9e66
JayQuack <95886074+JayQuack@users.noreply.github.com>
2025-02-10 19:57:29 -0500

    Message Validation for ChangeConfiguration added

--- backend/src/main/java/com/sim_backend/websockets/messages/ChangeConfiguration.java
+++ backend/src/main/java/com/sim_backend/websockets/messages/ChangeConfiguration.java
@@ -5,6 +5,7 @@ import com.sim_backend.websockets.annotations.OCPPMessageInfo;
 import com.sim_backend.websockets.types.OCPPMessage;
 import com.sim_backend.websockets.types.OCPPMessageRequest;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,10 +16,12 @@ import lombok.Setter;
 @EqualsAndHashCode(callSuper = true)
 @OCPPMessageInfo(messageCallID = OCPPMessage.CALL_ID_REQUEST, messageName = "ChangeConfiguration")
 public class ChangeConfiguration extends OCPPMessageRequest {
+  @Size(max = 50, message = "ChangeConfiguration key must not exceed 50 characters")
   @NotBlank(message = "ChangeConfiguration key is required")
   @SerializedName("key")
   private final String key;
 
+  @Size(max = 500, message = "ChangeConfiguration value must not exceed 500 characters")
   @NotBlank(message = "ChangeConfiguration value is required")
   @SerializedName("value")
   private final String value;
