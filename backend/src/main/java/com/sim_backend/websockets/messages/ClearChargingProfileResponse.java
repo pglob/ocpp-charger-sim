9a54d4ff5ad05e5881912d185fb5dbbf59af3e03
cfarhan <cameronfarhan2@gmail.com>
2025-02-23 17:40:07 -0800

    Create ClearChargingProfile request and response

---
+++ backend/src/main/java/com/sim_backend/websockets/messages/ClearChargingProfileResponse.java
@@ -0,0 +1,28 @@
+/**
+ * Represents an OCPP 1.6 Clear Charging Profile Response sent by a Charge Point in response to a
+ * clear charging profile request.
+ */
+package com.sim_backend.websockets.messages;
+
+import com.google.gson.annotations.SerializedName;
+import com.sim_backend.websockets.annotations.OCPPMessageInfo;
+import com.sim_backend.websockets.enums.ClearProfileStatus;
+import com.sim_backend.websockets.types.OCPPMessage;
+import com.sim_backend.websockets.types.OCPPMessageResponse;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+/** A OCPP Clear Charging Profile Response Message. */
+@AllArgsConstructor
+@Getter
+@EqualsAndHashCode(callSuper = true)
+@OCPPMessageInfo(
+    messageCallID = OCPPMessage.CALL_ID_RESPONSE,
+    messageName = "ClearChargingProfileResponse")
+public final class ClearChargingProfileResponse extends OCPPMessageResponse {
+
+  /** The status of the clear charging profile request. */
+  @SerializedName("status")
+  private final ClearProfileStatus status;
+}
