172abac722998393e96ac53b2df1dbe58a29b795
Paul Globisch <126132567+pglob@users.noreply.github.com>
2025-03-13 21:04:01 -0700

    Fixed issue where offline jobs would stack (#104)

--- backend/src/main/java/com/sim_backend/websockets/types/RepeatingTimedTask.java
+++ backend/src/main/java/com/sim_backend/websockets/types/RepeatingTimedTask.java
@@ -1,5 +1,6 @@
 package com.sim_backend.websockets.types;
 
+import com.sim_backend.websockets.OCPPWebSocketClient;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -8,17 +9,24 @@ public class RepeatingTimedTask extends TimedTask implements RepeatingTask {
   public long repeatDelay;
   public ChronoUnit unit;
   private final AtomicBoolean cancelled;
+  private OCPPWebSocketClient client;
 
   public boolean isCancelled() {
     return cancelled.get();
   }
 
   // Public constructor creates a new cancellation flag
-  public RepeatingTimedTask(ZonedDateTime time, long repeatTime, ChronoUnit unit, Runnable task) {
+  public RepeatingTimedTask(
+      ZonedDateTime time,
+      long repeatTime,
+      ChronoUnit unit,
+      Runnable task,
+      OCPPWebSocketClient client) {
     super(time, task);
     this.unit = unit;
     this.repeatDelay = repeatTime;
     this.cancelled = new AtomicBoolean(false);
+    this.client = client;
   }
 
   // Private constructor to propagate the cancellation flag
@@ -27,11 +35,13 @@ public class RepeatingTimedTask extends TimedTask implements RepeatingTask {
       long repeatTime,
       ChronoUnit unit,
       Runnable task,
-      AtomicBoolean cancelled) {
+      AtomicBoolean cancelled,
+      OCPPWebSocketClient client) {
     super(time, task);
     this.unit = unit;
     this.repeatDelay = repeatTime;
     this.cancelled = cancelled;
+    this.client = client;
   }
 
   @Override
@@ -39,9 +49,10 @@ public class RepeatingTimedTask extends TimedTask implements RepeatingTask {
     if (cancelled.get()) {
       return null; // Don't create a new task if cancelled
     }
-    ZonedDateTime nextExecutionTime = time.plus(this.repeatDelay, this.unit);
+    ZonedDateTime now = client.getScheduler().getTime().getSynchronizedTime();
+    ZonedDateTime nextExecutionTime = now.plus(this.repeatDelay, this.unit);
     return new RepeatingTimedTask(
-        nextExecutionTime, this.repeatDelay, this.unit, this.task, this.cancelled);
+        nextExecutionTime, this.repeatDelay, this.unit, this.task, this.cancelled, this.client);
   }
 
   // Method to cancel the repeating task
